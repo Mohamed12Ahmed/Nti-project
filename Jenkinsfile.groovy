0222d5d74d445376c4cc1a8d38aa43f26e9580e6
mohamed3213211 <162095127+mohamed3213211@users.noreply.github.com>
2024-04-29 20:53:43 +0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,50 +2,55 @@ pipeline {
     agent any
     
     environment {
-        IMAGE_NAME = "automation"
-        ECR_PASS = "730335448540.dkr.ecr.us-east-1.amazonaws.com"
-        ECR_URL = "730335448540.dkr.ecr.us-east-1.amazonaws.com"
-        AWS_REGION = "us-east-1"
-        IMAGE_TAG = "${IMAGE_NAME}:${BUILD_NUMBER}"
-        K8S_FILE = "k8s"
+        DOCKER_IMAGE_NAME = "project_app"  // Replace spaces with underscores or remove spaces
+        ECR_REPO_URL = "255058516901.dkr.ecr.us-east-1.amazonaws.com/ecr-repo"  // Update with your ECR repository URL
+        AWS_DEFAULT_REGION = "us-east-1"
+        K8S_YAML_FILE = "deployment.yml"  // Update with the correct path to your Kubernetes YAML file
+        DOCKER_TAG = "${DOCKER_IMAGE_NAME}:${BUILD_NUMBER}"
     }
 
     stages {
-        stage('Build Docker Image With Tag') {
+        stage('Build Docker image') {
             steps {
-                 script {
-                    echo "test"
-                    sh '''
-                    IMAGE_TAG="${IMAGE_TAG}"
-                    ls
-                    docker build -t ${ECR_URL}/${IMAGE_TAG} ./SimpleDjangoApp-master
-                    '''
+                script {
+                    echo "Building Docker image"
+                    sh "docker build -t ${DOCKER_IMAGE_NAME}:${BUILD_NUMBER} ."
                 }
             }
         }
-        
-        stage(' Login Aws ECR') {
+
+        stage('Authenticate Docker with ECR') {
             steps {
                 script {
-                    sh ''' 
-                    ECR_PASS="${ECR_PASS}"
-                    AWS_REGION="${AWS_REGION}"
-                    aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${ECR_PASS}
-                    '''
+                    echo "Authenticating Docker with ECR"
+                    sh "aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${ECR_REPO_URL}"
                 }
             }
         }
-        stage(' Push Docker Image to ECR ') {
+
+        stage('Tag Docker image') {
             steps {
                 script {
-                     withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: '730335448540']]){
-                    sh ''' 
-                    aws configure 
-                    ECR_URL="${ECR_URL}"
-                    echo "Push Docker image to ECR"
-                    docker push ${ECR_URL}/${IMAGE_TAG}
-                     '''
-                     }
+                    echo "Tagging Docker image"
+                    sh "docker tag ${DOCKER_IMAGE_NAME}:${BUILD_NUMBER} ${ECR_REPO_URL}:${BUILD_NUMBER}"
+                }
+            }
+        }
+
+        stage('Push Docker image to ECR') {
+            steps {
+                script {
+                    echo "Pushing Docker image to ECR"
+                    sh "docker push ${ECR_REPO_URL}:${BUILD_NUMBER}"
+                }
+            }
+        }
+
+        stage('Update Kubernetes YAML file') {
+            steps {
+                script {
+                    echo "Updating Kubernetes YAML file with ECR repository URL"
+                    sh "sed -i 's#image:.*#image: ${ECR_REPO_URL}:${BUILD_NUMBER}#' ${K8S_YAML_FILE}"
                 }
             }
         }
@@ -53,7 +58,8 @@ pipeline {
         stage('Deploy to Kubernetes') {
             steps {
                 script {
-                    sh "kubectl apply -f deployment k.yml "
+                    echo "Deploying to Kubernetes"
+                    sh "kubectl apply -f ${K8S_YAML_FILE}"
                 }
             }
         }
